791cbb1481c963975c92c5598ab5ef052211141c
Ivan Kalinichenko <ivan.kalinichenko@apliteni.com>
2024-04-14 19:42:57 +0400

    add taxes range

--- src/Taxes/Taxes.tsx
+++ src/Taxes/Taxes.tsx
@@ -1,17 +1,24 @@
-import { type FC, useState } from 'react';
+import { type FC } from 'react';
 
-const taxes = new Map<[number, number], number>([
-  [[1, 2], 100],
-  [[3, 4], 200],
-]);
+const taxes: Record<number, number> = {
+  7_703: 13.25,
+  11_623: 18,
+  16_472: 23,
+  21_321: 26,
+  27_146: 32.75,
+  39_791: 37,
+  51_997: 43.5,
+  81_199: 45.0,
+  [Number.MAX_SAFE_INTEGER]: 48.0,
+};
 
 export const Taxes: FC = () => {
   return (
     <div>
       <h1>Taxes</h1>
-      {Array.from(taxes).map(([key, value]) => (
+      {Object.entries(taxes).map(([key, value]) => (
         <div key={key[0]}>
-          <span>{key.join(', ')}:</span>
+          <span>{key}:</span>
           <span>{value}</span>
         </div>
       ))}

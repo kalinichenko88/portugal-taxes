d61d7db7cf201ed8391037ae53dff80d63a23580
Ivan Kalinichenko <ivan.kalinichenko@apliteni.com>
2024-04-16 13:18:34 +0400

    add styles

--- src/Taxes/Taxes.tsx
+++ src/Taxes/Taxes.tsx
@@ -1,27 +1,25 @@
 import { type FC } from 'react';
 
-const taxes: Record<number, number> = {
-  7_703: 13.25,
-  11_623: 18,
-  16_472: 23,
-  21_321: 26,
-  27_146: 32.75,
-  39_791: 37,
-  51_997: 43.5,
-  81_199: 45.0,
-  [Number.MAX_SAFE_INTEGER]: 48.0,
-};
+import { taxes } from './data';
+
+import './styles.css';
 
 export const Taxes: FC = () => {
+  Object.entries(taxes).forEach(([key, value]) => {
+    console.log(key, value);
+  });
+
   return (
     <div>
       <h1>Taxes</h1>
-      {Object.entries(taxes).map(([key, value]) => (
-        <div key={key[0]}>
-          <span>{key}:</span>
-          <span>{value}</span>
+      <div className="grid">
+        <div className="header">
+          <div>From</div>
+          <div>To</div>
+          <div>Percent</div>
         </div>
-      ))}
+        <div className="body">asd</div>
+      </div>
     </div>
   );
 };

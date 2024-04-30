3a2e5faa2c1ade76e83c0baf661f2be8e9171c1d
Ivan Kalinichenko <ivan.kalinichenko@apliteni.com>
2024-04-30 23:17:40 +0400

    move irs component to details

---
+++ src/components/Details/IRS/IRS.tsx
@@ -0,0 +1,15 @@
+import { type FC } from 'react';
+
+import { RangeItem } from './RangeItem/RangeItem';
+
+import styles from './styles.css';
+
+console.log({ styles });
+
+export const IRS: FC = () => {
+  return (
+    <div>
+      <h1>IRS</h1>
+    </div>
+  );
+};

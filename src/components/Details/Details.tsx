d431a1cfe978236fcc7d446dad1a86f716c6eb14
Ivan Kalinichenko <ivan.kalinichenko@apliteni.com>
2024-05-07 15:21:14 +0400

    fix the details layout

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -1,13 +1,15 @@
 import { type FC } from 'react';
 
 import { IRS } from './IRS';
+import { Social } from './Social';
 
 import './styles.css';
 
 export const Details: FC = () => {
   return (
-    <div className="h-full">
-      <div className="wrapper bg-indigo-400">
+    <div className="details">
+      <div className="wrapper">
+        <Social />
         <IRS />
       </div>
     </div>

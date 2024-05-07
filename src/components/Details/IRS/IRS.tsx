81e7d1e2feaa548c1ca1ec00ba42dae8c7cf4fae
Ivan Kalinichenko <ivan.kalinichenko@apliteni.com>
2024-05-07 15:43:10 +0400

    fix irs component

--- src/components/Details/IRS/IRS.tsx
+++ src/components/Details/IRS/IRS.tsx
@@ -1,26 +1,45 @@
 import { type FC } from 'react';
 
+import { useTaxService } from '@services/TaxService';
+import { Message } from '@components/Message';
+import { moneyFormatter } from '@helpers/formatter';
+
 import { RangeItem } from './RangeItem/RangeItem';
 
-import styles from './styles.css';
-
-console.log({ styles });
+import './styles.css';
 
 export const IRS: FC = () => {
+  const { irsResult } = useTaxService();
+
+  if (!irsResult) {
+    return null;
+  }
+
   return (
-    <div>
-      <h1>IRS</h1>
-      <ul className="menu bg-base-200 w-56 rounded-box">
-        <li>
-          <a>Item 1</a>
-        </li>
-        <li>
-          <a>Item 2</a>
-        </li>
-        <li>
-          <a>Item 3</a>
-        </li>
-      </ul>
+    <div className="irs">
+      <div className="header">
+        <h2>IRS</h2>
+
+        <Message>
+          The first year there is a <b>50%</b> discount on personal income tax (IRS), and in the second year there is a
+          <b>25%</b> discount.
+        </Message>
+      </div>
+
+      <div className="table">
+        {irsResult.ranges.map((range) => {
+          return (
+            <RangeItem key={range.withheld} range={range.taxRange} percent={range.percent} withheld={range.withheld} />
+          );
+        })}
+      </div>
+
+      <hr className="my-4" />
+
+      <div className="total">
+        <div>Total IRS:</div>
+        <div className="sum">{moneyFormatter.format(irsResult.withheld)}</div>
+      </div>
     </div>
   );
 };
--- src/components/Details/IRS/RangeItem/RangeItem.tsx
+++ src/components/Details/IRS/RangeItem/RangeItem.tsx
@@ -1,23 +1,32 @@
 import { type FC } from 'react';
 
 import { useTaxService } from '@services/TaxService';
+import { moneyFormatter, percentFormatter } from '@helpers/formatter';
+
+import './styles.css';
 
 type Props = {
   range: [number, number];
   percent: number;
+  withheld: number;
 };
 
-export const RangeItem: FC<Props> = ({ range, percent }) => {
+export const RangeItem: FC<Props> = ({ range, percent, withheld }) => {
   const [from, to] = range;
   const { income } = useTaxService();
   const tax = income > from;
 
+  const rangeFormatted =
+    to === Number.MAX_SAFE_INTEGER ? `From ${moneyFormatter.format(from)}` : moneyFormatter.formatRange(from, to);
+  const percentFormatted = percentFormatter.format(percent / 100);
+  const withheldFormatted = moneyFormatter.format(withheld);
+
   return (
-    <div>
-      <div>{range[0]}</div>
-      <div>{range[1]}</div>
-      <div>{percent}</div>
-      <div>asd</div>
+    <div className="item">
+      <div className="range">{rangeFormatted}</div>
+      <div className="percent">{percentFormatted}</div>
+      <div className="dots"></div>
+      <div className="sum">{withheldFormatted}</div>
     </div>
   );
 };

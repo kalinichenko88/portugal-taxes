a21486545f1fac54e880a24114c9f1ccff19a858
Ivan Kalinichenko <joker.bsg@gmail.com>
2024-05-11 18:05:18 +0400

    fix typo

--- src/components/Details/IRS/IRS.tsx
+++ src/components/Details/IRS/IRS.tsx
@@ -21,8 +21,7 @@ export const IRS: FC = () => {
         <h2>IRS</h2>
 
         <Message>
-          The first year there is a <b>50%</b> discount on personal income tax (IRS), and in the second year there is
-          a&nbsp;
+          The first year there is a <b>50%</b> discount on personal income tax (IRS), and in the second year there is a{' '}
           <b>25%</b> discount.
         </Message>
       </div>
